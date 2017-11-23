bc88c47fd9d71212eb7d3e1a10267b0d099d385f
KieranHolroyd <kieran@vizemedia.ga>
2017-11-23 15:52:13 +0000

    Made Changes

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PsiSyn Network | Home</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<script src="particles.min.js"></script>
+	<link rel="stylesheet" href="styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="grid/simple-grid.min.css">
+</head>
+<body>
+	<nav>
+		<ul>
+			<li></li>
+		</ul>
+	</nav>
+</body>
+</html>
