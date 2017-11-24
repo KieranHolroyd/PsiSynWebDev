053a857c9691ef59b7dd89667eea5cf8cf48fe53
KieranHolroyd <kieran@vizemedia.ga>
2017-11-24 17:32:51 +0000

    Acatually Got Started

--- index.php
+++ index.php
@@ -3,17 +3,29 @@
 <head>
 	<meta charset="UTF-8">
 	<title>PsiSyn Network | Home</title>
-	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
 	<script src="particles.min.js"></script>
 	<link rel="stylesheet" href="styles.css">
-	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 	<link rel="stylesheet" href="grid/simple-grid.min.css">
 </head>
 <body>
-	<nav>
-		<ul>
-			<li></li>
-		</ul>
-	</nav>
+	<div class="first">
+		<h1>PSISYN NETWORK</h1>
+	</div>
+	<div class="second">
+		<h1>About Us</h1>
+		<p>Hello and welcome to the PsiSyn Network launch! All server and TeamSpeak information can be found in the top right, and the only download file you need to join the server (@PSISYN addon) can be found from the links below! If you have any issues, feel free to join the TeamSpeak and wait in a technical support channel for help.
+<br>
+Use the below repo with Arma3Sync to download the mods required!
+<br>
+**NEW** Arma3Sync repo: ftp://144.217.78.20/.a3s/autoconfig
+<br>
+Keep in mind if you do purchase something from our store that currently the perks (reserved slot etc) have to be added manually, so we apologize to anyone who waits an extended amount of time, but again, just wait in the support channel and we will resolve the issue.
+<br>
+Also, if you do make content on the server (Twitch or YouTube), make sure to send an email to lewis@psisyn.com so we can share your content!
+<br>
+Thanks,<br>
+Lewis (Founder and CEO of PsiSyn Network Enterprises)</p>
+	</div>
 </body>
 </html>
